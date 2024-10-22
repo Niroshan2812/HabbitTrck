9a10e24f40db3cd7e0b7cda3719158f3f73800c5
Niroshan <niroshan9812@gmail.com>
2024-10-22 12:19:35 +0530

    Screen Implementation

--- HabbitTeack/components/HabitCard.js
+++ HabbitTeack/components/HabitCard.js
@@ -1,13 +1,68 @@
 // components/HabitCard.js
 
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import moment from 'moment';
 
 const HabitCard = ({ habit }) => {
+
+  const [completed, setCompleted] = useState(false);
+
+
+  //
+const getLast7Days =()=>{
+  const days =[];
+  for (let i = 6; i >=0; i--) {
+    days.push(moment().subtract(i,'days').format('DD MMM'));
+  }
+  return days;
+}
+
+  //function for mark habbitComplete 
+  const completeHabit = async ()=>{
+    try {
+      const updateHabbit  = {...habit, streak:habit.streak + 1};
+
+
+      //load existing habbit 
+      const storeHabbit = await AsyncStorage.getItem('habits');
+      const parseHabits = JSON.parse(storeHabbit);
+
+      // update habbit in list 
+      const updateHabbits = parseHabits.map(h => 
+        h.id === habit.id ? updateHabbit : h
+      );
+
+      //save updated habits to AsyncStore 
+      await AsyncStorage.setItem('habits', JSON.stringify(updateHabbits));
+
+      setCompleted(true);
+    } catch (error) {
+      consile.error("Error updating Habit ", error)
+    }
+  }
+
   return (
     <View style={styles.card}>
       <Text style={styles.habitName}>{habit.name}</Text>
       <Text style={styles.streak}>Streak: {habit.streak} days</Text>
+
+
+      {/* Display last 7 days progress*/}
+      <View style={styles.progress}>
+      
+
+
+      </View>
+      <TouchableOpacity style={completed ? styles.completeButton : styles.completeButton}
+      onPress={completeHabit}
+      disabled={completed}
+      > 
+      <Text style={styles.buttonText}>
+        {completed ? 'Completed' : 'Mark as Complete'}
+      </Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -30,6 +85,16 @@ const styles = StyleSheet.create({
     fontSize: 16,
     color: '#888',
     marginTop: 5,
+  },
+  completeButton: {
+    backgroundColor: '#aaa',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
   },
 });
 
